f03fe4dc30ed88dc9b77ed8d8e9674350212cd66
Sachinkrishna2000 <krishsachin2000@gmail.com>
2022-10-07 15:31:25 +0530

    commit

---
+++ TE_CSharp1/DictHash.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp1
+{
+    internal class DictHash
+    {
+        public static void Display_Dictionary()
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "Sam");
+            d.Add(2, "David");
+            d.Add(3, "John");
+            Console.WriteLine("DICTIONARY");
+            foreach (var obj in d)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------------------");
+            var key = new List<int>(d.Keys);
+            foreach (var obj in key)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------------------");
+            var values = new List<string>(d.Values);
+            foreach (var obj in values)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"Key: {d[1]}");
+            //Console.WriteLine($"Key: {vs[0]}");//If the key is not found -> raise an exception
+
+        }
+        /// <summary>
+        /// Hashtable -> class -> using System.Collections;
+        /// not type specific
+        /// key-value pair
+        /// </summary>
+
+        public static void Display_Hashtable()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "Rob");
+            ht.Add(2, 123);
+            ht.Add(3, "Charlie");
+            ht.Add(4, 1234);
+            ht.Add(5, "Alex");
+            ht.Add(6, 123456);
+            Console.WriteLine("HASHTABLE");
+            Console.WriteLine("-----------------------------------------");
+            //ICollection Interface
+            ICollection key = ht.Keys;
+            foreach (var obj in key)
+            {
+                Console.WriteLine(ht[obj]);
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"Key: {ht[1]}");
+            Console.WriteLine($"Key: {ht[2]}");//If the key is not found -> null value
+        }
+
+        public static void Main()
+        {
+           
+            Display_Dictionary();
+            Console.WriteLine("-----------------------------------------");
+            Display_Hashtable();
+        }
+    }
+}
---
+++ TE_CSharp1/Generics.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp1
+{
+    internal class Generics
+    {
+        // Declare Generic Delegate
+        public delegate T AreaDelegate<T>(T a);
+        public class Area
+        {
+            public int AreaSquare(int a)
+            {
+                return a * a;
+            }
+            public int AreaCube(int x)
+            {
+                return 6 * x * x;
+            }
+            public int AreaCircle(int r)
+            {
+                int b = Convert.ToInt32(Math.PI);
+                return b * r * r;
+            }
+            public double AreaSquare(double a)
+            {
+                return a * a;
+            }
+            public double AreaCube(double x)
+            {
+                return 6 * x * x;
+            }
+            public double AreaCircle(double r)
+            {
+                //int b = Convert.ToInt32(Math.PI);
+                return Math.PI * r * r;
+            }
+        }
+        class Result
+        {
+            public static void Main(string[] args)
+            {
+                Area ar = new Area();
+                Console.WriteLine("The Results Of Generic Delegates are-");
+                
+                // Instantiate delegate with AreaSquare method using int
+                AreaDelegate<int> dlgt = new AreaDelegate<int>(ar.AreaSquare);
+                Console.WriteLine("Area Of Square in int: " + dlgt(10));
+                // Instantiate delegate with AreaCube method
+                dlgt = ar.AreaCube;
+                Console.WriteLine("Area Of Cube in int: " + dlgt(10));
+
+                // Instantiate delegate with AreaCircle method
+                dlgt = ar.AreaCircle;
+                Console.WriteLine("Area Of Circle in int: " + dlgt(10));
+
+                // Instantiate delegate with AreaSquare method using double
+                AreaDelegate<double> dlgt1 = new AreaDelegate<double>(ar.AreaSquare);
+                Console.WriteLine("Area Of Square in double: " + dlgt1(10.5));
+
+                // Instantiate delegate with AreaCube method using double
+                dlgt1 = ar.AreaCube;
+                Console.WriteLine("Area Of Cube in double: " + dlgt1(10.5));
+
+                // Instantiate delegate with AreaCircle method using double
+                dlgt1 = ar.AreaCircle;
+                Console.WriteLine("Area Of Circle in double: " + dlgt1(10.5));
+
+                
+            }
+        }
+    }
+}
